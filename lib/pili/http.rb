0e196d71aaf6c0a63199ed6b783a8169a30c8dcc
Miclle <miclle.zheng@gmail.com>
2015-04-26 22:55:09 +0800

    Fix http api get auth header

--- lib/pili/http.rb
+++ lib/pili/http.rb
@@ -10,11 +10,14 @@ module Pili
     class << self
 
       def api_get(url)
-        signature_options = { :url => url, :method => "GET" }
+        signature_options = {
+          :url => url,
+          :method => "GET"
+        }
 
         encoded_sign = Auth.sign(Config.secret_key, Auth.generate_signature(signature_options))
 
-        headers = { "Authorization" => "Qiniu a #{Config.access_key}:#{encoded_sign}" }
+        headers = { "Authorization" => "Qiniu #{Config.access_key}:#{encoded_sign}" }
 
         get(url, :headers => headers)
       end
