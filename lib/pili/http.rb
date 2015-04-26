99d79b1e38fdb5f76f1bbeb7389b9e39465be263
Miclle <miclle.zheng@gmail.com>
2015-04-26 23:27:47 +0800

    Add http api post method.

--- lib/pili/http.rb
+++ lib/pili/http.rb
@@ -22,6 +22,26 @@ module Pili
         get(url, :headers => headers)
       end
 
+
+      def api_post(url, body)
+        signature_options = {
+          :url => url,
+          :content_type => "application/json",
+          :method => "POST",
+          :body => body
+        }
+
+        encoded_sign = Auth.sign(Config.secret_key, Auth.generate_signature(signature_options))
+
+        headers = {
+          "Authorization" => "Qiniu #{Config.access_key}:#{encoded_sign}",
+          "Content-Type"  => "application/json"
+        }
+
+        post(url, :headers => headers, :body => body.to_json)
+      end
+
+
     end
 
   end
