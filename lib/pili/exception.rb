7387cfd745557b1ab780d7197deec3bcb604d4aa
Jianjun Mao <justmao945@gmail.com>
2016-05-14 18:18:54 +0800

    pili v2

---
+++ lib/pili/exception.rb
@@ -0,0 +1,41 @@
+# coding: utf-8
+
+require "json"
+
+module Pili
+
+  class RPCError < Exception
+    
+    attr_reader :resp
+  
+    def initialize(message, resp)
+      @resp = resp
+      super(message)
+    end
+    
+    def code
+      resp.code
+    end
+    
+    def reqid
+      resp["X-Reqid"]
+    end
+    
+    def message
+      to_s
+    end
+    
+    def to_s
+      "#{super.to_s}: #{@resp.body}"
+    end
+  end
+  
+  # 资源已存在冲突错误
+  class ResourceConflict < RPCError
+  end
+  
+  # 资源不存在错误
+  class ResourceNotExist < RPCError
+  end
+  
+end
